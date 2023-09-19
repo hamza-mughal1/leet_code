4a57d1e60676840aa076a398a54bb7f4f0419ec8
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-09-19 20:41:36 +0500

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -8,22 +8,11 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def insert_at_end(self,val):
-        if self.head == None:
-            node = Node(val,None)
-            self.head = node
-            return
-        node = self.head
-        while node:
-            if node.next == None:
-                node.next = Node(val,None)
-                return
-            node = node.next
-    
     def print(self):
         if self.head == None:
             print("linked_list is empty!")
             return
+        
         lis = []
         node = self.head
         while node:
@@ -31,6 +20,16 @@ class LinkedList:
             node = node.next
         print(lis)
         return
+    
+    def len(self):
+        num = 0
+        node = self.head
+        while node:
+            if node.next == None:
+                return num
+            node = node.next
+            num+=1
+        return num
 
     def insert_at_beginning(self,val):
         if self.head == None:
@@ -42,15 +41,18 @@ class LinkedList:
         self.head = node
         return
 
-    def len(self):
-        num = 0
+    def insert_at_end(self,val):
+        if self.head == None:
+            node = Node(val,None)
+            self.head = node
+            return
+        
         node = self.head
         while node:
             if node.next == None:
-                return num
+                node.next = Node(val,None)
+                return
             node = node.next
-            num+=1
-        return num
 
     def insert_at_index(self,val,index):
         if (self.head == None) or (0 > index) or  (index > self.len()+1):
@@ -65,16 +67,37 @@ class LinkedList:
             num+=1
             node = node.next
 
-linked_list = LinkedList()
-linked_list.insert_at_end(5)
-linked_list.insert_at_end(6)
-linked_list.insert_at_end(7)
-linked_list.insert_at_end(5)
-linked_list.insert_at_end(6)
-linked_list.insert_at_end(7)
-linked_list.insert_at_beginning("h")
-linked_list.insert_at_beginning("i")
-linked_list.insert_at_index("t", 0)
+    def insert_after_value(self,value,previous_value):
+        if self.head == None:
+            raise ValueError("invalid argument!")
 
-linked_list.print()
-print(linked_list.len())
+        node = self.head 
+        while node:
+            if node.val == previous_value:
+                node.next = Node(value,node.next)
+                return
+            node = node.next
+
+        raise ValueError("invalid argument, value not found!")
+    
+    def insert_values (self,values):
+            for i in values:
+                self.insert_at_end(i)
+            return
+
+    def remove_by_value(self,value):
+        if self.head == None:
+            raise ValueError("invalid argument!")
+
+        node = self.head
+        while node:
+            if node.next.val == value:
+                node.next = node.next.next
+                return
+            node = node.next
+    
+    
+
+        
+
+linked_list = LinkedList()
