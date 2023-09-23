37f9b0013c03b4fa50259baa4a76e303fc074224
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-09-23 22:58:06 +0500

    Create Best_Time_to_Buy_and_Sell_Stock.py

---
+++ Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,13 @@
+def foo(prices):
+    l = 0
+    maximum = 0
+    if len(prices) <= 1:
+        return maximum
+    for r in range(1,len(prices)):
+        if prices[r] < prices[l]:
+            l = r
+        if prices[l] < prices[r]:
+            maximum = max(maximum,prices[r]-prices[l])
+        
+    return maximum
+
